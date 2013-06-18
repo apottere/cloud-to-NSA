17d8206565f0b0b799ba4941cbc194eefd33263d
Andrew Potter <apottere@gmail.com>
2013-06-18 18:50:18 -0400

    Renamed crx file.

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,15 +31,23 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "The NSA");
-	v = v.replace(/\bThe cloud\b/g, "The NSA");
-	v = v.replace(/\bthe Cloud\b/g, "the NSA");
-	v = v.replace(/\bthe cloud\b/g, "the NSA");
-	
 	v = v.replace(/\bCloud Computing\b/g, "NSA Datamining");
 	v = v.replace(/\bcloud Computing\b/g, "NSA Datamining");
 	v = v.replace(/\bCloud computing\b/g, "NSA datamining");
 	v = v.replace(/\bcloud computing\b/g, "NSA datamining");
+
+	v = v.replace(/\bcloud computing\b/gi, "NSA datamining");
+	v = v.replace(/\bcloudcomputing\b/gi, "NSAdatamining");
+	
+
+	v = v.replace(/\bThe Cloud\b/g, "The NSA");
+	v = v.replace(/\bThe cloud\b/g, "The NSA");
+	v = v.replace(/\bthe Cloud\b/g, "the NSA");
+	v = v.replace(/\bthe cloud\b/g, "the NSA");
+
+	v = v.replace(/\bthe cloud\b/gi, "the NSA");
+	v = v.replace(/\bthecloud\b/gi, "theNSA");
+	
 	
 	textNode.nodeValue = v;
 }
